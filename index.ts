7afed74c9f4bbe62a730237b310525c18b431499
Mhammadkhan17 <mhammadkhan740@gmail.com>
2024-05-03 16:43:31 +0500

    Created Currency Converter

---
+++ index.ts
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+
+const currency :any = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+}
+
+let userAnswer = await inquirer.prompt(
+    [
+        {
+            name:"from",
+            message:"Select the currency you want to convert from",
+            type:"list",
+            choices:["USD", "EUR", "GBP", "INR", "PKR"]
+        },
+        {
+            name:"to",
+            message:"Select the currency you want to convert into",
+            type:"list",
+            choices:["USD", "EUR", "GBP", "INR", "PKR"]
+        },
+        {
+            name:"amount",
+            message:"Enter the amount:",
+            type:"number",
+        }
+    ]
+)
+
+let fromAmount = currency[userAnswer.from]
+let toAmount = currency[userAnswer.to]
+let amount = userAnswer.amount
+let baseAmount = amount / fromAmount
+let convertedAmount =  baseAmount * toAmount;
+let exchangeRate = (Math.round(convertedAmount * 100) / 100);
+console.log(exchangeRate)
